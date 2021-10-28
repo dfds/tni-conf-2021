1b8bf35d6481dafc249f5fd60a61dffc9cd51843
Emil H. Clausen <sequoiia@hummel.yt>
2021-10-28 19:57:19 +0200

    Updated text formatting

--- app/src/components/LandingPage.vue
+++ app/src/components/LandingPage.vue
@@ -13,7 +13,7 @@
         <h1>Welcome</h1>
 
 <p>Dear colleague
-
+<br />
 Welcome to the TECH MOVES US T&I Conference. We are so excited to finally welcome you for our very first conference in three years.
 
 We kindly ask you to look at the practical information below regarding the upcoming event.
@@ -23,9 +23,11 @@ We kindly ask you to look at the practical information below regarding the upcom
 
 <p>The ship is docked at the DFDS Terminal in Dampfærgevej 30. (street address).
 
-1. If you arrive by your own car, please park at DFDS House. Remember to register at the PLOT app.
-2. If you arrive by train, the S-train stops at the nearest station called Nordhavn. When you get out of the station, turn left and walk under the railway. On the other side of the road, you can see the terminal and the ship.
-3. If you arrive by taxi, tell the driver to take you to Oslobåden at Dampfærgevej 30.
+<ul>
+  <li> <span style="font-weight: 700">1.</span> If you arrive by your own car, please park at DFDS House. Remember to register at the PLOT app.</li>
+  <li> <span style="font-weight: 700">2.</span> If you arrive by train, the S-train stops at the nearest station called Nordhavn. When you get out of the station, turn left and walk under the railway. On the other side of the road, you can see the terminal and the ship.</li>
+  <li> <span style="font-weight: 700">3.</span> If you arrive by taxi, tell the driver to take you to Oslobåden at Dampfærgevej 30.</li>
+</ul>
 
 </p>
 
