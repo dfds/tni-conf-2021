a97338394cf04fc0f51a0caf6167839dbeaf8c14
janwiberg-dfds <44774288+janwiberg-dfds@users.noreply.github.com>
2021-11-03 09:47:42 +0100

    Update LandingPage.vue

--- app/src/components/LandingPage.vue
+++ app/src/components/LandingPage.vue
@@ -7,18 +7,11 @@
     </div>
     <div class="banner"><img src="https://dfds-ti-conf-data.s3.eu-central-1.amazonaws.com/tech-moves-us.jpg" /></div>
     <div class="text">
-      <p>Dear colleague<br />Welcome to the TECH MOVES US T&I Conference. We are so excited to finally welcome you for our very first conference in three years. We kindly ask you to look at the practical information below regarding the upcoming event.</p>
-      <h2>Where to meet the 2nd of November</h2>
+      <p>Dear colleague<br />Welcome to the TECH MOVES US T&I Conference. We hope you are enjoying the conference and invite you to study the conference program at the links above.</p>
+      <h2>Regarding dinner on Day 2</h2>
       <p>
-        The ship is docked at the DFDS Terminal in Dampfærgevej 30. (street address).
-        <ul>
-          <li> <span style="font-weight: 700">1.</span> If you arrive by your own car, please park at DFDS House. Remember to register at the PLOT app.</li>
-          <li> <span style="font-weight: 700">2.</span> If you arrive by train, the S-train stops at the nearest station called Nordhavn. When you get out of the station, turn left and walk under the railway. On the other side of the road, you can see the terminal and the ship.</li>
-          <li> <span style="font-weight: 700">3.</span> If you arrive by taxi, tell the driver to take you to Oslobåden at Dampfærgevej 30.</li>
-        </ul>
+        Day 2 is a formal dinner at 20.00 in Seven Seas. Please see seating assignments on the posters near the restaurant, or click the link to the seating plan PDF above.
       </p>
-      <h2>Check in and boarding</h2>
-      <p>You can arrive for check-in at the terminal from 8:30 CET. You MUST arrive no later than 9:30 CET. As we will swipe everyone’s passport to get all the needed passenger information for the manifest, we recommend that you come as early as possible to avoid long lines before boarding. Please remember to bring your passport or national ID.</p>
     </div>
   </div>
 </template>
